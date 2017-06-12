502aa918f0e30b465d20751e0aff4ca8cb121c82
robmarkcole <robmarkcole@gmail.com>
2017-06-12 07:27:01 +0100

    Match bus class
    still need to fix device_state_attributes

--- train_class.py
+++ train_class.py
@@ -1,4 +1,4 @@
-# ? Init with a dict, e.g. {Home: MAL}
+# As per bus but route becomes origin_name, direction becomes destination_name, next_suses becomes next_trains
 
 class UkTransportLiveTrainTimeSensor(UkTransportSensor):
     """Live train time sensor from UK transportapi.com."""
@@ -8,11 +8,15 @@ class UkTransportLiveTrainTimeSensor(UkTransportSensor):
         """Construct a live bus time sensor."""
         self._station_code = station_code         # stick to the naming convention of transportAPI
         self._destination_name = destination_name
-        self._next_train = {}
+        self._next_trains = {}
+        self._destination_re = re.compile(
+            '{}'.format(destination_name), re.IGNORECASE
+        )
 
         sensor_name = 'Next train to {}'.format(destination_name)
         query_url =  'train/station/{}/live.json'.format(station_code)
 
+        print(query_url)
         # also requires '&darwin=false&destination=WAT&train_status=passenger'
 
         UkTransportSensor.__init__(
@@ -24,8 +28,41 @@ class UkTransportLiveTrainTimeSensor(UkTransportSensor):
         params = {'darwin': 'false', 'destination': self._destination_name, 'train_status': 'passenger'}
 
         self._do_api_request(params)
-        departure = self._data['departures']['all'][0]    # next train
-        self._next_train = {
-            'aimed_arrival_time': departure['aimed_arrival_time'],
-            'status': departure['status']
-        }     
+
+        if self._data != {}:
+            self._next_trains = []
+
+            for departure in self._data['departures']['all']:      # don't need a regex search as passing in destination to search
+                #print_json(departure)   # uncomment to see all fields
+                self._next_trains.append({
+                    'origin_name': departure['origin_name'],
+                    'destination_name': departure['destination_name'],
+                    'status': departure['status'],
+                    'scheduled': departure['aimed_departure_time'],
+                    'estimated': departure['expected_departure_time'],
+                    'platform': departure['platform'],
+                    'operator_name': departure['operator_name']
+                    })
+
+            self._state = min(map(
+                _delta_mins, [train['scheduled'] for train in self._next_trains]
+            ))
+
+    @property
+    def device_state_attributes(self):
+        """Return other details about the sensor state."""
+        if self._data is not None:
+            attrs = {ATTR_ATTRIBUTION: ATTRIBUTION}  # {'attribution': 'Data provided by transportapi.com'}
+            for key in [
+                    ATTR_ATCOCODE, ATTR_LOCALITY, ATTR_STOP_NAME,
+                    ATTR_REQUEST_TIME
+            ]:
+                attrs[key] = self._data.get(key)           # place these attributes
+            attrs[ATTR_NEXT_BUSES] = self._next_buses      # not in
+            print_json(attrs)
+            return attrs
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit this state is expressed in."""
+        return "min" 
