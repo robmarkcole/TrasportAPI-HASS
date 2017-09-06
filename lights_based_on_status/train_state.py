07a94e1963236df566fb7f6084bbd0689c8bac49
robmarkcole <robmarkcole@gmail.com>
2017-09-06 07:44:38 +0100

    Rewrite trains_state.py

--- lights_based_on_status/train_state.py
+++ lights_based_on_status/train_state.py
@@ -1,35 +1,62 @@
-############################################
-# Enter your your sensor name here.        #
-############################################
-entity_id = 'sensor.next_train_to_wim'
-scheduled = False   # Check if train scheduled
-train_time = '07:15'
+"""
+Script to receive alerts for train of interest (TOI).
+"""
+SCHEDULED = False
 
-if hass.states.get(entity_id).state == "No departures":
-  logger.warn('No Departures')
+# Set config
+MAIN_ENTITY_ID = 'sensor.next_train_to_wim'
+TOI_ENTITY_ID = 'sensor.morning_train'
+TOI_TIME = '07:15'
+
+# Set notification details
+MSG_TITLE = "Train Status Update"
+NOTIFY_ID = 'robins_and_marias_iphones'
+
+
+if hass.states.get(MAIN_ENTITY_ID).state == "No departures":
+    logger.warn('No Departures')
 else:
-  attributes = hass.states.get(entity_id).attributes
-  morning_train_status = hass.states.get('sensor.morning_train').state
-  try:
-    for train in attributes['next_trains']:
-      if train['scheduled'] == train_time:
-          scheduled = True   # Check if train scheduled
-          if train['status'] != morning_train_status:
-              hass.states.set('sensor.morning_train', train['status'])
+    attributes = hass.states.get(MAIN_ENTITY_ID).attributes
+    try:
+        for train in attributes['next_trains']:
+            if train['scheduled'] == TOI_TIME:
+                SCHEDULED = True
+                train_status = train['status']
 
-              if train['status'] == 'ON TIME':
-                  hass.services.call('light', 'turn_on', { "entity_id" : 'light.lamp', 'color_name': 'green' })
-                  hass.services.call('notify', 'robins_and_marias_iphones', { "title": "Train Status Update", "message": "The {} is scheduled to be on time.".format(train_time)})
-              elif train['status'] == 'LATE':
-                  hass.services.call('light', 'turn_on', { "entity_id" : 'light.lamp', 'color_name': 'orange' })
-                  hass.services.call('notify', 'robins_and_marias_iphones', { "title": "Train Status Update", "message": "The {} will be late and is now sheduled at".format(train_time) + train['estimated']})
-              elif train['status'] == 'CANCELLED':
-                  hass.services.call('light', 'turn_on', { "entity_id" : 'light.lamp', 'color_name': 'red' })
-                  hass.services.call('notify', 'robins_and_marias_iphones', { "title": "Train Status Update", "message": "The {} has been cancelled.".format(train_time)})
-          break
-  except:
-    logger.warn('Failed to get next trains')
+                if train['status'] != hass.states.get(TOI_ENTITY_ID).state:
+                    logger.warn('States equal')
+                    hass.states.set(TOI_ENTITY_ID, train_status)
 
-if not scheduled:
-    hass.states.set('sensor.my_early_train', 'No data')  # check no data
+                    if train_status == 'ON TIME':
+                        hass.services.call('light', 'turn_on', {
+                            "entity_id": 'light.lamp', 'color_name': 'green'})
+
+                        hass.services.call('notify', NOTIFY_ID, {
+                            "title": MSG_TITLE,
+                            "message": "The {} is scheduled to be on time."
+                            .format(TOI_TIME)})
+
+                    elif train_status == 'LATE':
+                        hass.services.call('light', 'turn_on', {
+                            "entity_id": 'light.lamp', 'color_name': 'orange'})
+
+                        hass.services.call('notify', NOTIFY_ID, {
+                            "title": MSG_TITLE,
+                            "message": "The {} will be late and ETA is"
+                            .format(TOI_TIME) + train['estimated']})
+
+                    elif train_status == 'CANCELLED':
+                        hass.services.call('light', 'turn_on', {
+                            "entity_id": 'light.lamp', 'color_name': 'red'})
+
+                        hass.services.call('notify', NOTIFY_ID, {
+                            "title": MSG_TITLE,
+                            "message": "The {} has been cancelled."
+                            .format(TOI_TIME)})
+                break
+    except:
+        logger.warn('Error in train_state.py')
+
+if not SCHEDULED:
+    hass.states.set(TOI_ENTITY_ID, 'No data')  # check no data
 #logger.warn('Test complete')
